63e76f816867bb4f24bb0a4353c21b18f7d8c017
Jake Leuthardt <jakeleuthardt1@gmail.com>
2026-03-16 20:43:19 -0500

    i forgot to commit this

---
+++ emulator.py
@@ -0,0 +1,13 @@
+import pyte
+
+class TerminalEmulator:
+    def __init__(self, rows=24, cols=80):
+        self._screen = pyte.Screen(cols, rows)
+        self._stream = pyte.Stream(self._screen)
+
+        def feed(self, data: bytes):
+            text = data.decode("utf-8", errors="replace")
+            self._stream.feed(text)
+
+        def resize(self, rows, cols):
+            self._screen.resize(rows, cols)
